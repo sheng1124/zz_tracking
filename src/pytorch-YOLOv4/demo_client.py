f05b8ae5787291aa93b2369e66a858d7a3117c8d
qbgbmpty <qbgbmpty@gmail.com>
2021-12-10 11:43:22 +0800

    yolo client

--- src/pytorch-YOLOv4/demo_client.py
+++ src/pytorch-YOLOv4/demo_client.py
@@ -1,19 +1,5 @@
 # -*- coding: utf-8 -*-
-'''
-@Time          : 20/04/25 15:49
-@Author        : huguanghao
-@File          : demo.py
-@Noice         :
-@Modificattion :
-    @Author    :
-    @Time      :
-    @Detail    :
-'''
 
-# import sys
-# import time
-# from PIL import Image, ImageDraw
-# from models.tiny_yolo import TinyYoloNet
 from tool.utils import *
 from tool.torch_utils import *
 from tool.darknet2pytorch import Darknet
@@ -29,40 +15,6 @@ import cv2
 
 """hyper parameters"""
 use_cuda = True
-
-def detect_cv2(cfgfile, weightfile, imgfile):
-    import cv2
-    m = Darknet(cfgfile)
-
-    m.print_network()
-    m.load_weights(weightfile)
-    print('Loading weights from %s... Done!' % (weightfile))
-
-    if use_cuda:
-        m.cuda()
-
-    num_classes = m.num_classes
-    if num_classes == 20:
-        namesfile = 'data/voc.names'
-    elif num_classes == 80:
-        namesfile = 'data/coco.names'
-    else:
-        namesfile = 'data/x.names'
-    class_names = load_class_names(namesfile)
-
-    img = cv2.imread(imgfile)
-    sized = cv2.resize(img, (m.width, m.height))
-    sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
-
-    for i in range(2):
-        start = time.time()
-        boxes = do_detect(m, sized, 0.4, 0.6, use_cuda)
-        finish = time.time()
-        if i == 1:
-            print('%s: Predicted in %f seconds.' % (imgfile, (finish - start)))
-
-    plot_boxes_cv2(img, boxes[0], savename='predictions.jpg', class_names=class_names)
-
 
 def detect_cv2_camera(cfgfile, weightfile):
     
@@ -104,42 +56,6 @@ def detect_cv2_camera(cfgfile, weightfile):
         #cv2.waitKey(1)
         ccc.send_result(boxes[0])
 
-
-
-def detect_skimage(cfgfile, weightfile, imgfile):
-    from skimage import io
-    from skimage.transform import resize
-    m = Darknet(cfgfile)
-
-    m.print_network()
-    m.load_weights(weightfile)
-    print('Loading weights from %s... Done!' % (weightfile))
-
-    if use_cuda:
-        m.cuda()
-
-    num_classes = m.num_classes
-    if num_classes == 20:
-        namesfile = 'data/voc.names'
-    elif num_classes == 80:
-        namesfile = 'data/coco.names'
-    else:
-        namesfile = 'data/x.names'
-    class_names = load_class_names(namesfile)
-
-    img = io.imread(imgfile)
-    sized = resize(img, (m.width, m.height)) * 255
-
-    for i in range(2):
-        start = time.time()
-        boxes = do_detect(m, sized, 0.4, 0.4, use_cuda)
-        finish = time.time()
-        if i == 1:
-            print('%s: Predicted in %f seconds.' % (imgfile, (finish - start)))
-
-    plot_boxes_cv2(img, boxes, savename='predictions.jpg', class_names=class_names)
-
-
 class Client():
     def __init__(self):
         SERVER = "163.25.103.111"
@@ -147,6 +63,7 @@ class Client():
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect((SERVER, PORT))
         self.client.sendall(bytes("video_detect",'UTF-8'))
+        self.client.recv(1)
 
         self.payload = ">L"
         self.payload_size = struct.calcsize(self.payload)
@@ -181,14 +98,11 @@ class Client():
 
 def get_args():
     parser = argparse.ArgumentParser('Test your image or video by trained model.')
-    parser.add_argument('-cfgfile', type=str, default='./cfg/yolov4.cfg',
+    parser.add_argument('-cfgfile', type=str, default='../../data/cfg/yolov4.cfg',
                         help='path of cfg file', dest='cfgfile')
     parser.add_argument('-weightfile', type=str,
-                        default='./weight/yolov4.weights',
+                        default='../../data/weights/yolov4.weights',
                         help='path of trained model.', dest='weightfile')
-    parser.add_argument('-imgfile', type=str,
-                        default='./data/image.jpg',
-                        help='path of your image file.', dest='imgfile')
     args = parser.parse_args()
 
     return args
