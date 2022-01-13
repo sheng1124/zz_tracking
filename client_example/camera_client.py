7b9683a8cb103f22537d2e6eec144f4fab2be16e
sheng1124 <rhythm7321@gmail.com>
2022-01-13 14:45:26 +0800

    gggg

--- client_example/camera_client.py
+++ client_example/camera_client.py
@@ -9,6 +9,8 @@ if __name__ == '__main__':
     #"rtsp://admin:ppcb1234@192.168.154.15:554/unicast/c7/s1/live") #640 480
     cam_id = 0
     cam = cv2.VideoCapture(cam_id)
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
     while True:
         try:
